80f8da4b14ae6e2f1bb336da79d09e258d1b3b6e
acornies <andrew.cornies@hashicorp.com>
2024-06-05 22:28:50 -0400

    Add some validation on hcp inputs

--- variables.tf
+++ variables.tf
@@ -1,28 +1,36 @@
 variable "region" {
-    type = string
-    default = "us-east-2"
+  type    = string
+  default = "us-east-2"
 }
 
 variable "event_name" {
-    type = string
-    default = "your-gameday-name"
-}
-
-variable "event_hcp_project_id" {
-    type = string
+  type    = string
+  default = "your-gameday-name"
+  validation {
+    condition     = can(regex("^[-a-zA-Z0-9_]+$", var.event_name))
+    error_message = "Event name must only contain alphanumeric characters, dashes, and underscores."
+  }
 }
 
 variable "cloud_provider" {
-    type = string
-    default = "aws"
+  type    = string
+  default = "aws"
+  validation {
+    condition     = can(regex("^(aws|azure)$", var.cloud_provider))
+    error_message = "Cloud provider must be one of 'aws', or 'azure'."
+  }
 }
 
 variable "hcp_vault_tier" {
-    type = string
-    default = "dev"
+  type    = string
+  default = "dev"
+  validation {
+    condition     = can(regex("^(dev|starter_small|standard_small)$", var.hcp_vault_tier))
+    error_message = "HCP Vault tier must be one of 'dev', 'starter_small', or 'standard_small'."
+  }
 }
 
 variable "hcp_vault_public_endpoint" {
-    type = bool
-    default = "true"
+  type    = bool
+  default = "true"
 }
